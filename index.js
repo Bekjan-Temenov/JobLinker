c7c8aae36260dd6ce08b5897c29f205531ee4ba9
Temenov-Bekjan <temenovbekzan@gmail.com>
2025-10-01 20:36:26 +0400

    first commit

--- index.js
+++ index.js
@@ -2,27 +2,22 @@ const TelegramBot = require('node-telegram-bot-api');
 const { TelegramClient } = require('telegram');
 const { StringSession } = require('telegram/sessions');
 
-// Токен твоего бота
 const botToken = '7812065409:AAFlm3oewp2ZuO4BLSABvv0y6VZzo0BxkMQ';
 const bot = new TelegramBot(botToken, { polling: true });
 
-// Данные для Telegram Client
 const apiId = 25282310;
 const apiHash = 'e49f0e092c1e6f2a30ac59b31f3a7c93';
-const stringSession = new StringSession('1AgAOMTQ5LjE1NC4xNjcuNTEBu7/CJv/fDenh4JfDRmHlJ3j0ET1Bzy6cxpiU0oRXVrUt5vbeKVOH21LpyWlynX0zOJ2YnEEDDbKavUa4sXCYs/y+JUgJL6xStoH9yfzqT8jfFmwc5xOwpGLMGpJaIvLa9yATjIfIQKnc8IWsbdNKhczcNTrv9sf19KkbEo1vp2wPuZlvFA/GWzfn9L3EOpUWGuIHzUix6kvW9xF3Oyg9DWZdEBUD1OG23XOruJdNWWsJJ/+A79cRpnwFU63EOMrEXS/xir3azgdU3YGj2V2jvS5g8blPE8akyQr9ueXf/nG+HpSCoRTJ+U0wqlRemrk2EWVKHgmxysTPu+57MqO/HUg='); // Пока пустая, вставим позже
+const stringSession = new StringSession('1AgAOMTQ5LjE1NC4xNjcuNTEBu7/CJv/fDenh4JfDRmHlJ3j0ET1Bzy6cxpiU0oRXVrUt5vbeKVOH21LpyWlynX0zOJ2YnEEDDbKavUa4sXCYs/y+JUgJL6xStoH9yfzqT8jfFmwc5xOwpGLMGpJaIvLa9yATjIfIQKnc8IWsbdNKhczcNTrv9sf19KkbEo1vp2wPuZlvFA/GWzfn9L3EOpUWGuIHzUix6kvW9xF3Oyg9DWZdEBUD1OG23XOruJdNWWsJJ/+A79cRpnwFU63EOMrEXS/xir3azgdU3YGj2V2jvS5g8blPE8akyQr9ueXf/nG+HpSCoRTJ+U0wqlRemrk2EWVKHgmxysTPu+57MqO/HUg='); 
 
-// Список каналов
-const channels = ['@devkg' ,'@remote','@codifynews','@easy_frontend_jobs'];
+const channels = ['@devkg', '@remote', '@codifynews', '@JScript_jobs', '@runello_rus_webdevelopment', '@job_react', '@easy_frontend_jobs', '@forfrontend', '@remote'];
 
-// Ключевые слова
-const keywords = ['frontend', 'frontend junior', 'frontend middle', 'react', 'javascript',  'Frontend Developer','vue','next.js'];
+const keywords = ['frontend', 'frontend junior', 'frontend middle', 'react', 'javascript',  'Frontend Developer','next.js', 'Frontend разработчик', 'Frontend'];
 
-// Твой Telegram ID
 const chatId = 6808962395;
 
 const client = new TelegramClient(stringSession, apiId, apiHash, { connectionRetries: 5 });
 
-const sentMessages = new Set(); // Используем Set для уникальности ID
+const sentMessages = new Set(); 
 
 async function startBot() {
     try {
@@ -49,7 +44,6 @@ async function startBot() {
                             console.log('message.text', message.text);
                             bot.sendMessage(chatId, `Найдена вакансия в ${channel}:\n${message.text}`);
                             
-                            // Добавляем ID сообщения в Set, чтобы не отправить его снова
                             sentMessages.add(message.id);
                         }
                     }
@@ -63,7 +57,6 @@ async function startBot() {
     }
 }
 
-// Функция для ввода кода
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
@@ -77,8 +70,6 @@ function prompt(question) {
     });
 }
 
-// Запуск бота
 startBot().catch(console.error);
 
-// Периодический запуск (каждый час)
 setInterval(startBot, 3600000);
